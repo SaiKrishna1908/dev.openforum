67f55cfb1a9e63474900559b63ae9bbd88e90462
Sai Krishna <saikrishna05h1@gmail.com>
2020-05-30 23:33:35 +0530

    Intial Commit.

---
+++ src/main/java/com/dev/openforum/OpenforumApplication.java
@@ -0,0 +1,13 @@
+package com.dev.openforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenforumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/openforum/OpenforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.openforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenforumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
