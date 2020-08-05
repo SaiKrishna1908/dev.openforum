7f268b74f8ab911f30573c28cf357f0581b8a2a2
Sai Krishna <saikrishna05h1@gmail.com>
2020-08-05 21:22:18 +0530

    Gitub: added postgres support

---
+++ openforumschema.sql
@@ -0,0 +1,2 @@
+create table template (id  bigserial not null, description varchar(255), priority int4 not null, status varchar(255), primary key (id))
+-- create table template (id  bigserial not null, description varchar(255), priority int4 not null, status varchar(255), primary key (id))
--- src/main/java/com/dev/openforum/DataLoader/DataFetcher.java
+++ src/main/java/com/dev/openforum/DataLoader/DataFetcher.java
@@ -5,7 +5,7 @@ import com.dev.openforum.Service.TemplateService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public class DataFetcher implements CommandLineRunner {
 
     private final TemplateService templateService;
